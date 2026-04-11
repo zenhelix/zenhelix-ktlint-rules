c976164f45e2ada5537137b681103c55e7346b11
dmitriimedakin <dm.medakin.online@gmail.com>
2026-04-11 20:38:17 +0200

    feat: add EditorConfig infrastructure to ZenhelixRule base class
    Add usesEditorConfigProperties parameter and lineLengthSettings property
    to ZenhelixRule, preparing the base class for EditorConfig integration.
    Rules can now access line length thresholds via lineLengthSettings.

--- zenhelix-ktlint-rules-core/src/main/kotlin/io/github/zenhelix/ktlint/rules/ZenhelixRule.kt
+++ zenhelix-ktlint-rules-core/src/main/kotlin/io/github/zenhelix/ktlint/rules/ZenhelixRule.kt
@@ -4,16 +4,22 @@ import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 import com.pinterest.ktlint.rule.engine.core.api.Rule
 import com.pinterest.ktlint.rule.engine.core.api.RuleAutocorrectApproveHandler
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
+import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfigProperty
 
 public abstract class ZenhelixRule(
     ruleId: RuleId,
     visitorModifiers: Set<VisitorModifier> = emptySet(),
+    usesEditorConfigProperties: Set<EditorConfigProperty<*>> = emptySet(),
 ) : Rule(
     ruleId = ruleId,
     about = ZENHELIX_ABOUT,
     visitorModifiers = visitorModifiers,
+    usesEditorConfigProperties = usesEditorConfigProperties,
 ),
     RuleAutocorrectApproveHandler {
+
+    protected var lineLengthSettings: LineLengthSettings = LineLengthSettings()
+        private set
 
     protected inline fun emitAndCorrect(
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision,
